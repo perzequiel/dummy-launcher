83379260d2914bdfd420d1914df5c911cc02f960
Ezequiel Gonzalez <ezequiel@pow-gaming.com>
2023-02-03 21:12:53 -0300

    fixed

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get('/game/:gameId', async (req, res) => {
 // storing game list
 app.get('/game-list.js', async (req, res) => {
   const { body } = await requestPromise(POW_GAMING__GAME_LIST)  
-  return res.end(`const gamesList = ${body.games}`);
+  return res.end(`const gamesList = ${JSON.stringify(JSON.parse(body).games)}`);
 })
 
 app.use(express.static('site'));

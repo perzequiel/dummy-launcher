d59ac4a274aca7b1c9e739c889a1799fc7b0cf6e
Ezequiel Gonzalez <ezequiel@pow-gaming.com>
2023-03-21 11:15:37 -0300

    fixed

--- site/funcional.js
+++ site/funcional.js
@@ -14,6 +14,19 @@ const recoverUser = ()=>{
 const setGames = async ()=>{
     for (const game of gamesList) {
         gameBtn = document.querySelectorAll(".btn-" + game.class_name)
+        container = document.querySelectorAll(".games-container")
+
+        if (container.length) {
+            container[0].innerHTML = container[0].innerHTML + `<div class="game game__blackjack">
+                <div class="game-logo">
+                    <span>${game.name}</span>
+                </div>
+                <div class="btn-${game.class_name} game-bg">
+                    <img src="${game.image}" alt="blackjaack-background">
+                </div>
+                <div class="btn btn-The21BlackJack" >PLAY NOW!</div>
+            </div>`
+        }
 
         if (gameBtn.length) {
             gameBtn[0].addEventListener('click', ()=>{
